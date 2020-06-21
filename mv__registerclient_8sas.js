4e44ca946c549e74036cb53ca97bf6bb8165e73f
Allan Bowe <>
2020-06-22 00:26:59 +0200

    build.sh build on 2020-06-22:00:26:59

--- mv__registerclient_8sas.js
+++ mv__registerclient_8sas.js
@@ -1,5 +1,4 @@
 var mv__registerclient_8sas =
 [
-    [ "mp_abort", "mv__registerclient_8sas.html#a9c8d0d7333b07462d24a095f73b33120", null ],
     [ "fname2", "mv__registerclient_8sas.html#ae23d22ff117b17f8ef79be811b69b9a7", null ]
 ];
